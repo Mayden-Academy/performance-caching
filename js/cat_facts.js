512a1b9679646e44047f9f2956c59f710c53ce7a
Mike Oram <mike.oram@mayden.co.uk>
2020-07-14 10:35:43 +0100

    simplify data structure

--- js/cat_facts.js
+++ js/cat_facts.js
@@ -5,12 +5,12 @@
     const facts = await response.json();
     const factContainer = document.querySelector('#catFacts');
 
-    facts.data.forEach((factObj, i) => {
+    facts.data.forEach((fact, i) => {
         factContainer.innerHTML += `<div class="col-3">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Cat fact ${++i}</h5>
-                    <p class="card-text">${factObj.fact}</p>
+                    <p class="card-text">${fact}</p>
                 </div>
             </div>
         </div>`
