809ddd2de8bcf8e5b6a285724a0e5046e03dcd46
Mike Oram <mike.oram@mayden.co.uk>
2020-07-14 10:25:45 +0100

    cached cat facts in localStorage

--- js/cat_facts.js
+++ js/cat_facts.js
@@ -1,20 +1,42 @@
 
 
 (async function() {
-    const response = await fetch('catfacts.json');
-    const facts = await response.json();
+    const facts = await fetchData('catfacts.json');
     const factContainer = document.querySelector('#catFacts');
 
-    facts.data.forEach((factObj, i) => {
+    facts.forEach((fact, i) => {
         factContainer.innerHTML += `<div class="col-3">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Cat fact ${++i}</h5>
-                    <p class="card-text">${factObj.fact}</p>
+                    <p class="card-text">${fact}</p>
                 </div>
             </div>
         </div>`
     })
 
 }())
+
+async function fetchData(url) {
+
+    let data = {data: []}
+    let i = 1;
+    while (localStorage.getItem('fact' + i) !== null) {
+        i++
+        data.data.push(localStorage.getItem('fact' + i))
+    }
+
+    if (data.data.length == 0) {
+        const response = await fetch(url);
+        data = await response.json();
+
+        data.data.forEach((fact, i) => {
+            ++i;
+            localStorage.setItem('fact' + i, fact);
+        })
+
+    }
+
+    return data.data;
+}
 
